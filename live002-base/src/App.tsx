1ff096e5dec42f4a96691e757015db6706957032
Luis Fernando <luis.silva9902@gmail.com>
2023-10-26 22:16:24 -0300

    feat: create store

--- live002-base/src/App.tsx
+++ live002-base/src/App.tsx
@@ -2,6 +2,7 @@ import { AppBar } from './components/AppBar';
 import { TodosList } from './components/TodosList';
 import { GlobalProvider } from './contexts/GlobalContext';
 import { useRenderCounter } from './hooks/useRenderCounter';
+import './store/createStore';
 
 export function App() {
   useRenderCounter('App');
---
+++ live002-base/src/store/createStore.ts
@@ -0,0 +1,58 @@
+type SetterFn<T> = (prevState: T) => Partial<T>;
+
+export function createStore<TState>(initialState: TState) {
+  let state = initialState;
+
+  const listeners = new Set<() => void>();
+
+  function notifyListeners() {
+    listeners.forEach((listener) => listener());
+  }
+
+  function setState(partialState: Partial<TState> | SetterFn<TState>) {
+    const newValue =
+      typeof partialState === 'function' ? partialState(state) : partialState;
+
+    state = {
+      ...state,
+      ...newValue,
+    };
+
+    notifyListeners();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  function subscribe(listener: () => void) {
+    listeners.add(listener);
+
+    return () => {
+      listeners.delete(listener);
+    };
+  }
+
+  return {
+    setState,
+    getState,
+    subscribe,
+  };
+}
+
+const store = createStore({
+  userName: '',
+  active: false,
+  counter: 1,
+});
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.setState({
+  userName: 'Luis',
+});
+store.setState((prevState) => ({
+  counter: prevState.counter + 1,
+}));
